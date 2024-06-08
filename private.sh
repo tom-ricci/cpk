3e01bf292199a8b079c395674b7781cd4113ca64
Thomas Ricci <me@thomasricci.dev>
2024-06-08 04:09:48 -0400

    tested!

--- private.sh
+++ private.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Check if a private key is provided as an argument
-if [ -z "$1" ] && [ -z "$2" ]; then
+if [ -z "$1" ] || [ -z "$2" ]; then
     echo "Usage: $0 \$PRIVATE_KEY \$PUBLIC_KEY"
     exit 1
 fi
 
 # Variables!!
 PRIVATE_KEY=$1
-PUBLIC_KEY=$( echo $2 cut -c 5-)
+PUBLIC_KEY=$( echo $2 | cut -c 5-)
 PRIVATE_KEY_FILE="$HOME/.ssh/id_$( echo $PUBLIC_KEY | awk '{print $1}' )"
 
 # Ensure the user isn't trying to get help
@@ -20,8 +20,16 @@ fi
 # Ensure the .ssh directory exists
 mkdir -p "$HOME/.ssh"
 
+# Regex for spaces in a string, needed for file checking
+pattern=" |'"
+
 # Determine if the input is a file or string
-if [ -f $PRIVATE_KEY ]; then
+if [[ $PRIVATE_KEY =~ $pattern ]]; then
+
+    # Write the private key to the id_rsa file
+    echo "$PRIVATE_KEY" > "$PRIVATE_KEY_FILE"
+
+elif [ -f $PRIVATE_KEY ]; then
 
     # Copy the private key to the id_rsa file
     cp "$PRIVATE_KEY" "$DEST_PRIVATE_KEY"
@@ -32,6 +40,9 @@ else
     echo "$PRIVATE_KEY" > "$PRIVATE_KEY_FILE"
 
 fi
+
+# Save public key to .pub file
+echo "ssh-$PUBLIC_KEY" > "$PRIVATE_KEY_FILE.pub"
 
 # Set the correct permissions
 chmod 700 "$HOME/.ssh"
